d389785e32e0b5566f48c102997c27cbd1e00acd
Ahmed Osama <67006758+aosama314@users.noreply.github.com>
2022-04-02 17:01:47 +0200

    Build login and register UI pages (#1)
    Co-authored-by: Ahmed Osama Mosbah <ahmed.mosbah@vodafone.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import Login from "./components/Login";
+import TabsPanel from "./common/TabsPanel";
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <Row>Online Shopping</Row>
+        <Row className="justify-content-md-center mt-5">
+          <Col xs={12} md={12} lg={6}>
+            <TabsPanel />
+          </Col>
+        </Row>
+        <Row></Row>
       </Container>
     </div>
   );
